27f985a241cd250ed1b37e23974021d2be4a4ab1
Pavel Argentov <argentoff@gmail.com>
2010-10-30 23:28:13 +0400

    Merge all changes to rsh.rb
    Merged all experimental code into rsh.rb, removed rsh_p_r.rb.

--- lib/rsh.rb
+++ lib/rsh.rb
@@ -40,21 +40,21 @@
 #   % man 1 rsh
 #
 class Rsh
+  require 'socket'
+  # flags whether to force pure ruby rsh client
+  attr_accessor :ruby_impl
+  # Local user name, used in pure ruby implementation.
+  attr_accessor :luser
   # path to rsh program, +String+
   attr_reader   :executable
-
   # result +String+
   attr_reader   :result
-
   # remote server hostname or IP, +String+
   attr_accessor :host
-
   # remote command, +String+
   attr_accessor :command
-
   # remote username, +String+
   attr_accessor :ruser
-
   # rsh timeout, +Integer+ (see man 1 rsh)
   attr_accessor :to
 
@@ -62,47 +62,62 @@ class Rsh
   # information
   attr_accessor :nullr
 
-  # The Constructor. Checks the presence of rsh in the system (running,
+  # The Constructor. If :ruby_impl is false checks the presence of rsh in the system (running,
   # naturally, 'which rsh') and prepares the command to be run with <tt>execute!</tt>. 
   # rsh CLI arguments are either having default values, being collected from constructor 
   # call or specified via accessors.
   #
   # Call sequence:
   #   Rsh.new -> Rsh instance
-  #   Rsh.new(:host => "hostname", :command => "example.sh", :ruser => "jack", :to => 5, :nullr => true) -> Rsh instance
+  #   Rsh.new(:host      => "hostname",
+  #           :command   => "example.sh",
+  #           :ruser     => "jack",
+  #           :to        => 5,
+  #           :nullr     => true
+  #           :ruby_impl => true) -> Rsh instance
   # 
   # If called without arguments, the following default values are being used:
   # 
-  #   :host    => "localhost"
-  #   :command => ""
-  #   :ruser   => "nobody"
-  #   :to      => 3 
-  #   :nullr   => false
+  #   :host      => "localhost"
+  #   :command   => ""
+  #   :ruser     => "nobody"
+  #   :to        => 3
+  #   :nullr     => false
+  #   :ruby_impl => false
   #
   def initialize(args={})
-    args = {:host    => "localhost", 
-            :command => "", 
-	    :ruser   => "nobody",
-	    :to      => 3, 
-	    :nullr   => false}.merge!(args)
-    begin
-      open("| which rsh") do |io|
-        @executable = io.gets.chomp
+    args = {
+      :host      => "localhost",
+      :command   => "",
+      :ruser     => "nobody",
+      :luser     => ENV["USER"] || "nobody",
+      :to        => 3,
+      :nullr     => false,
+      :ruby_impl => false
+    }.merge!(args)
+
+    @result     = ""
+    @ruby_impl  = args[:ruby_impl]
+    @host       = args[:host]
+    @command    = args[:command]
+    @ruser      = args[:ruser]
+    @to         = args[:to]
+    @nullr      = args[:nullr]
+
+    unless @ruby_impl
+      begin
+        open("| which rsh") do |io|
+          @executable = io.gets.chomp
+        end
+      rescue => detail
+        @ruby_impl = true
       end
-    rescue => detail
-      raise "FATAL: Could not find rsh executable!"
     end
-    
-    @host    = args[:host]
-    @command = args[:command]
-    @ruser   = args[:ruser]
-    @to      = args[:to]
-    @nullr   = args[:nullr]
-    @result  = ""
   end
 
-
-  # Executes rsh command using previously collected arguments. 
+  # Executes rsh command using previously collected arguments. If :ruby_impl is
+  # true or system rsh binary was not found during initialize runs pure ruby rsh
+  # client.
   #
   # If given a block, calls it for each line received from rsh output
   # (parameter _line_).
@@ -125,13 +140,42 @@ class Rsh
   def execute!(command=nil)
     @command = command if command
     @result = ""
-    open "|#{@executable} #{"-n" if @nullr} -l#{@ruser} -t#{@to} #{@host} #{@command}" do |io|
-      io.each do |line|
+    if @ruby_impl
+      # pure ruby implementation call
+      rsh_ruby do |line|
         yield(line) if block_given?
         @result << line
       end
+    else
+      # OS'es rsh(1) call
+      open "|#{@executable} #{"-n" if @nullr} -l#{@ruser} -t#{@to} #{@host} #{@command}" do |io|
+        io.each do |line|
+          yield(line) if block_given?
+          @result << line
+        end
+      end
     end
     @result
+  end
+
+  private
+  # pure ruby rsh client
+  def rsh_ruby
+    r = ""
+    s = TCPSocket.new(@host, 514)
+    s.write "0\0"
+    s.write "#{@luser}\0"
+    s.write "#{@ruser}\0"
+    s.write "#{@command}\0"
+    while line = s.gets
+      if block_given?
+        yield line
+      else
+        r << line
+      end
+    end
+    s.close
+    r == "" ? nil : r
   end
 
   alias :execute :execute!
