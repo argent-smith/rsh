6db51813be00f2170821f1347015936223646122
Pavel Argentov <argentoff@gmail.com>
2011-09-09 19:51:50 +0400

    Removed that silly example

--- spec/rshc_spec.rb
+++ spec/rshc_spec.rb
@@ -5,10 +5,6 @@ describe Rshc do
     it "returns the system's rsh path" do
       Rshc.find.should == `which rsh`.chomp
     end
-    it "can be mocked" do
-      Rshc.stub(:find).and_return(:value)
-      Rshc.find.should == :value
-    end
   end
 end
 
